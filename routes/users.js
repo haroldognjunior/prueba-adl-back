7e1c78b797c38261e2b0feb7d8792d22e03f9693
Júnior Nogueira <haroldognjunior@gmail.com>
2024-05-08 20:30:40 -0600

    feat: changed response on users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("Estás viendo la respuesta de usuarios");
 });
 
 module.exports = router;
